fbb138f91e78679323edd698f23bf94065f99198
Elvis Pontes <elvis-pontes@hotmail.com>
2023-08-21 13:25:47 -0300

    test in jobs computer

--- web/js/draw.js
+++ web/js/draw.js
@@ -8,5 +8,8 @@ draw.path =(ctx, path, color='black')=>{
     for(let i=0; i<path.length; i++){
           ctx.lineTo(... path[i]);
     }
+
+    ctx.lineCap = "round";
+    ctx.leneJoin = "round";
     ctx.stroke(); 
 }
