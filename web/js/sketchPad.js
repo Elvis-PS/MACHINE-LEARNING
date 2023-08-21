8e76f823d9832551f9391ab4df5a1a6036eee23d
Elvis Pontes <elvis-pontes@hotmail.com>
2023-08-21 16:31:52 -0300

    button undo added

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -10,6 +10,14 @@ class SketchPad{
         `;
         container.appendChild(this.canvas);
 
+        const lineBreak=document.createElement('br');
+        container.appendChild(lineBreak);
+
+        this.undoBtn = document.createElement('button');
+        this.undoBtn.innerHTML = "UNDO";
+        container.appendChild(this.undoBtn);
+
+
         this.paths=[];
         this.isDrawing = false;
         this.ctx = this.canvas.getContext("2d");
@@ -31,6 +39,23 @@ class SketchPad{
         }
         this.canvas.onmouseup=()=>{
             this.isDrawing=false;
+        }
+        this.canvas.ontouchstart=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousedown(loc);
+        }
+        this.canvas.ontouchmove=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousemove(loc);
+        }
+        this.canvas.ontouchend=()=>{
+            this.canvas.onmouseup();
+        }
+        this.undoBtn.onclick=(evt)=>{
+            this.paths.pop();
+            this.#redraw();
+
+            this.paths.length>0?this.undoBtn.disabled=false:this.undoBtn.disabled=true;
         }
     }
 
