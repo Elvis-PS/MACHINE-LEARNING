8b51325177ccd06f8a20654cc0e074f06659ac65
Elvis Pontes <elvis-pontes@hotmail.com>
2023-08-26 17:04:37 -0300

    samples file created and understood

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -9,26 +9,27 @@ constants.SAMPLES=constants.DADTASET_DIR+ "/samples.json";
 
 const fs= require('fs');
 
+//used to read directory files
 const fileNames= fs.readdirSync(constants.RAW_DIR);
 const samples =[];
 let id=1;
 
-fileNames.forEach(fn=>{
-    const content = fs.readFileSync(constants.RAW_DIR+"/"+fn);
+fileNames.forEach((fileName)=>{
+    //method used to read the content of a file
+    const content = fs.readFileSync(constants.RAW_DIR+'/'+fileName);
     const {session, student, drawings} = JSON.parse(content);
-    for (let label in drawings){
+    for(let label in drawings){
         samples.push({
             id,
             label,
-            student_name: student,
-            student_id: session,
-        })
+            student_name:student,
+            student_id:session,
+        });
         id++;
     }
-});
-
-//desconstruct the json element into variables.
+})
 
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
+
 
  
